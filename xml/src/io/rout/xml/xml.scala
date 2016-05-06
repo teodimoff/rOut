7ce89fe0c325b1317e6ce2ab27acb00ff6f6a561
Teodimoff <teodimoff@gmail.com>
2016-05-06 18:33:01 +0300

    style

--- xml/src/io/rout/xml/xml.scala
+++ xml/src/io/rout/xml/xml.scala
@@ -1,41 +1,44 @@
 package io.rout.generic
 
-
+import cats.data.Xor
+import com.twitter.io.Buf
+import com.twitter.util.Try
+import io.rout.{Decode, Encode, Error}
 import io.rout.parse.xml._
 import shapeless._
+import scala.xml.XML
 
 object xml {
 
-/*
-  object auto {
-
-  implicit def deriveEncoder[T,LKV](implicit
-    gen:LabelledGeneric.Aux[T,LKV],
-    toXml: Lazy[ToXml.Wrap[LKV]]
-  ): ToXml[T] = ToXml.deriveInstance(gen,toXml)
-
-  implicit def deriveDecoder[T,LKV](implicit
-    gen:LabelledGeneric.Aux[T,LKV],
-    fromXml: Lazy[FromXml.Wrap[LKV]]
-  ): FromXml[T] = FromXml.deriveInstance(gen,fromXml)
-}
- */
-
 object semiauto {
 
-  class Derive[T] {
-   def xmlEncoder[LKV](implicit
-    gen:LabelledGeneric.Aux[T,LKV],
-    toXml: Lazy[ToXml.Wrap[LKV]]
-  ): ToXml[T] = ToXml.deriveInstance(gen,toXml)
+  class Encoder[T]{
+    def encodeXml(implicit toXml: ToXml[T]): Encode.ApplicationXml[T] = Encode.xml(t =>
+      Buf.Utf8(toXml.toXml(t).toString()))
 
-   def xmlDecoder[LKV](implicit
+    def derive[LKV](implicit
+      gen:LabelledGeneric.Aux[T,LKV],
+      toXml: Lazy[ToXml.Wrap[LKV]]
+    ): ToXml[T] = ToXml.deriveInstance(gen,toXml)
+  }
+
+  class Decoder[T]{
+     def decodeXml(implicit fromXml: FromXml[T]): Decode.ApplicationXml[String,T] = Decode.applicationXml(i =>
+      fromXml.fromXml(XML.loadString(i)).fold[Xor[Error,Try[T]]](
+        err => Xor.Left(Error(err.getMessage)),
+        value => Xor.Right(Try(value))
+      ))
+
+   def derive[LKV](implicit
     gen:LabelledGeneric.Aux[T,LKV],
     fromXml: Lazy[FromXml.Wrap[LKV]]
   ): FromXml[T] = FromXml.deriveInstance(gen,fromXml)
+
   }
 
-  def derive[T] = new Derive[T]
+  def decode[T] = new Decoder[T]
+
+  def encode[T] = new Encoder[T]
 }
 
 }
